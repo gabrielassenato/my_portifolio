2e2c1f4b0b111cf594c7d24be785d610e10dca27
Gabriel Assenato <gabrielassenato@gmail.com>
2024-08-09 18:10:00 -0300

    :sparkles: feat(footer): Adicionando rodapé

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import { FooterBar } from './styles'
+
+export const Footer = () => (
+  <FooterBar>
+    <span>Todos os direitos reservados @gabriel - 2024</span>
+  </FooterBar>
+)
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const FooterBar = styled.div`
+  background-color: #ddd;
+  margin-top: 42px;
+  text-align: center;
+  padding: 18px;
+`
