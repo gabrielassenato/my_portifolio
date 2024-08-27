de21d960bab8ea3346af11c507b167c7937a0c6c
Gabriel Assenato <gabrielassenato@gmail.com>
2024-08-27 12:20:49 -0300

    :recycle: refator(myprojects): Refatorei a forma como são processados os meus projetos na tela

--- src/components/MyProjects/index.tsx
+++ src/components/MyProjects/index.tsx
@@ -1,51 +1,123 @@
-import mockup from '../../assets/mockup.png'
+import blade from '../../assets/mock-blade.png'
+import efood from '../../assets/mock-efood.png'
+import egames from '../../assets/mock-egames.png'
+import fc from '../../assets/mock-fc.png'
+import finans from '../../assets/mock-finans.png'
+import medias from '../../assets/mockup-calculadoraMedia.png'
+import agenda from '../../assets/mockup-agendaContatos.png'
+import niver from '../../assets/mock-birthday.png'
+import sorteador from '../../assets/mock-sorteador.png'
+import tattoo from '../../assets/mock-tattoo.png'
+import toDo from '../../assets/mock-toDoList.png'
 import reactLogo from '../../assets/logo_react.png'
 import nodeLogo from '../../assets/logo_nodejs.png'
-import githubLogo from '../../assets/logo_github_white.png'
-import vercelLogo from '../../assets/logo_vercel_white.png'
+import reactCss from '../../assets/logo_css3.png'
+import nodeGrunt from '../../assets/logo_grunt.png'
 import seeMore from '../../assets/add.png'
-import {
-  AboutProject,
-  MyProjectsContainer,
-  MyProjectsProjects,
-  Tecnology,
-  VisitTheProject
-} from './styles'
+import seeLess from '../../assets/remove.png'
+import { MoreProjectsDiv, MyProjectsContainer } from './styles'
 import { Button } from '../../styles'
 
-export const MyProjects = () => (
-  <MyProjectsContainer>
-    <h1>Projetos</h1>
-    <MyProjectsProjects>
-      <img src={mockup} alt="" />
-      <AboutProject>
-        <h2>Titulo</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
-          quos? Cum asperiores fugiat quisquam, qui dolor nulla! Tenetur non
-          unde eaque perferendis esse excepturi necessitatibus itaque aliquid
-          ex? Eius, iusto.
-        </p>
-        <Tecnology>
-          Tecnologias:
-          <img src={reactLogo} alt="" />
-          <img src={nodeLogo} alt="" />
-        </Tecnology>
-        <VisitTheProject>
-          <h5>Visite o projeto:</h5>
-          <div>
-            <Button>
-              Repositório <img src={githubLogo} alt="" />
-            </Button>
-            <Button>
-              Ver projeto <img src={vercelLogo} alt="" />
-            </Button>
-          </div>
-        </VisitTheProject>
-      </AboutProject>
-    </MyProjectsProjects>
-    <Button>
-      Ver mais projetos <img src={seeMore} alt="" />
-    </Button>
-  </MyProjectsContainer>
-)
+import { CarouselProjects } from '../CarouselProjects'
+import { useState } from 'react'
+import { MoreProjects } from '../MoreProjects'
+
+const projects = [
+  {
+    title: 'Franck Comunica',
+    description:
+      'Esse projeto eu fiz para um tatuador Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet officia ullam voluptatum fugit et delectus, ratione assumenda ex similique dolorum officiis ipsa, facere ipsam minus aliquam cum natus. Quibusdam',
+    imgSrcTecnology: [reactLogo, reactCss, nodeGrunt],
+    imgSrcMockup: fc,
+    imgAlt: 'logo grunt'
+  },
+  {
+    title: 'Efood',
+    description:
+      'Esse porjeto é para um restaurante Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet officia ullam voluptatum fugit et delectus, ratione assumenda ex similique dolorum officiis ipsa, facere ipsam minus aliquam cum natus. Quibusdam.',
+    imgSrcTecnology: [reactLogo, nodeLogo],
+    imgSrcMockup: efood,
+    imgAlt: 'logo grunt'
+  },
+  {
+    title: 'Eplay',
+    description:
+      'Este projeto é para uma loja de games Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet officia ullam voluptatum fugit et delectus, ratione assumenda ex similique dolorum officiis ipsa, facere ipsam minus aliquam cum natus. Quibusdam.',
+    imgSrcTecnology: [reactLogo],
+    imgSrcMockup: egames,
+    imgAlt: 'logo grunt'
+  },
+  {
+    title: 'Agenda de contatos',
+    description:
+      'Este projeto é para uma loja de games Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet officia ullam voluptatum fugit et delectus, ratione assumenda ex similique dolorum officiis ipsa, facere ipsam minus aliquam cum natus. Quibusdam.',
+    imgSrcTecnology: [reactLogo],
+    imgSrcMockup: agenda,
+    imgAlt: 'logo grunt'
+  },
+  {
+    title: 'Calculadora de medias',
+    description:
+      'Este projeto é para uma loja de games Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet officia ullam voluptatum fugit et delectus, ratione assumenda ex similique dolorum officiis ipsa, facere ipsam minus aliquam cum natus. Quibusdam.',
+    imgSrcTecnology: [reactLogo],
+    imgSrcMockup: medias,
+    imgAlt: 'logo grunt'
+  }
+]
+
+const moreProjects = [
+  {
+    name: 'Blade Runner',
+    img: blade,
+    imgTecnology: [reactLogo]
+  },
+  {
+    name: 'To do list',
+    img: toDo,
+    imgTecnology: [reactLogo]
+  },
+  {
+    name: 'Assenato Tattoo',
+    img: tattoo,
+    imgTecnology: [reactLogo]
+  },
+  {
+    name: 'Sorteador',
+    img: sorteador,
+    imgTecnology: [reactLogo]
+  },
+  {
+    name: 'Finans',
+    img: finans,
+    imgTecnology: [reactLogo]
+  },
+  {
+    name: 'Contador Aniversario',
+    img: niver,
+    imgTecnology: [reactLogo]
+  }
+]
+
+export const MyProjects = () => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [isToggled, setIsToggled] = useState(false)
+
+  const handleToggle = () => {
+    setIsVisible(!isVisible)
+    setIsToggled(!isToggled)
+  }
+
+  return (
+    <MyProjectsContainer>
+      <h1>Projetos</h1>
+      <CarouselProjects items={projects} />
+      <Button type="button" onClick={handleToggle}>
+        Ver mais projetos
+        <img src={isToggled ? seeLess : seeMore} alt="" />
+      </Button>
+      <MoreProjectsDiv className={isVisible ? 'isVisibles' : ''}>
+        <MoreProjects items={moreProjects} />
+      </MoreProjectsDiv>
+    </MyProjectsContainer>
+  )
+}
--- src/components/MyProjects/styles.ts
+++ src/components/MyProjects/styles.ts
@@ -1,68 +1,27 @@
 import styled from 'styled-components'
-import { colors } from '../../styles'
 
 export const MyProjectsContainer = styled.div`
   background-color: #eee;
   margin-top: 24px;
   border-radius: 74px 22px 74px 22px;
   padding: 34px;
-`
 
-export const MyProjectsProjects = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  margin-bottom: 44px;
-
-  img {
-    height: 400px;
+  h1 {
+    margin-bottom: 16px;
   }
 `
 
-export const AboutProject = styled.div`
-  padding-top: 84px;
+export const MoreProjectsDiv = styled.div`
+  margin-top: 32px;
+  max-height: 0;
+  overflow: hidden;
+  opacity: 0;
+  visibility: hidden;
+  transition: max-height 1s ease, opacity 1s ease, visibility 1s ease;
 
-  h2 {
-    font-size: 28px;
-    text-align: center;
-  }
-
-  p {
-    margin: 8px 0;
-  }
-`
-
-export const Tecnology = styled.p`
-  display: flex;
-  align-items: center;
-
-  img {
-    height: 30px;
-    margin-left: 8px;
-  }
-`
-
-export const VisitTheProject = styled.div`
-  margin-top: 8px;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-
-  h5 {
-    margin-bottom: 0px;
-  }
-
-  div {
-    display: flex;
-    justify-content: center;
-  }
-
-  button {
-    margin: 8px;
-    padding: 4px;
-    width: 160px;
-
-    img {
-      height: 20px;
-    }
+  &.isVisibles {
+    max-height: 1000px;
+    opacity: 1;
+    visibility: visible;
   }
 `
