620db67add55d7b2ce14cdc58e25a51961b9ebc6
Gabriel Assenato <gabrielassenato@gmail.com>
2024-09-17 12:32:23 -0300

    :lipstick: feat(moreprojects): Estilização dos projetos

--- src/components/MoreProjects/styles.ts
+++ src/components/MoreProjects/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { breakpoints } from '../../styles'
 
 export const MoreProjectsContainer = styled.div`
   h1 {
@@ -18,20 +19,67 @@ export const MoreProjectsContainer = styled.div`
       text-align: center;
     }
 
+    .image-container {
+      position: relative;
+      overflow: hidden;
+    }
+
     img {
       height: 200px;
       width: 100%;
+      display: block;
+      transition: opacity 0.8s ease;
     }
 
-    div {
-      justify-content: center;
-      text-align: center;
+    .overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(0, 0, 0, 0.5);
+      color: #fff;
       display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      opacity: 0;
+      transition: opacity 0.8s ease;
+      text-align: center;
+      padding: 10px;
+    }
 
-    button {
-      margin: 4px;
-      padding: 4px;
-      width: 40%;
+    .image-container:hover .overlay {
+      opacity: 1;
+    }
+
+    .overlay a {
+      margin: 6px 0;
+      width: 80%;
+    }
+  }
+
+  div {
+    justify-content: center;
+    text-align: center;
+    display: flex;
+
+    a {
+      justify-content: center;
+      display: flex;
+      width: 100%;
+
+      button {
+        margin: 6px 0px;
+        padding: 4px;
+        width: 80%;
+      }
+    }
+  }
+
+  @media (max-width: ${breakpoints.cellphone}) {
+    ul {
+      display: block;
     }
   }
 `
