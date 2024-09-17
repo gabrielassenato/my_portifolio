a92893ed66c103f041c5f3116a38b25aebe2d367
Gabriel Assenato <gabrielassenato@gmail.com>
2024-09-17 12:22:10 -0300

    :pencil: text(footer): pequena alteração na ordem do texto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,6 @@ import { FooterBar } from './styles'
 
 export const Footer = () => (
   <FooterBar>
-    <span>Todos os direitos reservados @gabriel - 2024</span>
+    <span>Todos os direitos reservados. &copy; 2024 Gabriel A.</span>
   </FooterBar>
 )
