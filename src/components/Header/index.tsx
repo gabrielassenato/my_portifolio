8c89c0fc3adc029007c923ec65e00935766eb032
Gabriel Assenato <gabrielassenato@gmail.com>
2024-09-17 12:18:59 -0300

    :sparkles: feat(header): Adicionei um menu hamburguer para dispositivos móveis e configurei as rotas dos links do cabeçalho para as respectivas sections

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,67 @@
-import { Avatar, HeaderBar, LinkItem, Links } from './styles'
+import {
+  Avatar,
+  Burguer,
+  HeaderBar,
+  HeaderRow,
+  LinkItem,
+  Links,
+  NavMobile
+} from './styles'
 import avatar from '../../assets/avatar.png'
+import { useState } from 'react'
+useState
 
-const Header = () => (
-  <HeaderBar>
-    <Avatar>
-      <img src={avatar} alt="Avatar Gabriel" />
-      <h1>Gabriel Assenato</h1>
-    </Avatar>
-    <nav>
-      <Links>
-        <LinkItem>
-          <a href="#">Sobre</a>
-        </LinkItem>
-        <LinkItem>
-          <a href="#">Habilidades</a>
-        </LinkItem>
-        <LinkItem>
-          <a href="#">Projetos</a>
-        </LinkItem>
-        <LinkItem>
-          <a href="#">Contatos</a>
-        </LinkItem>
-      </Links>
-    </nav>
-  </HeaderBar>
-)
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <HeaderBar>
+      <HeaderRow>
+        <div>
+          <Avatar>
+            <img src={avatar} alt="Avatar Gabriel" />
+            <h1>Gabriel Assenato</h1>
+          </Avatar>
+          <Burguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Burguer>
+          <nav>
+            <Links>
+              <LinkItem>
+                <a href="#about">Sobre</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#skills">Habilidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#projects">Projetos</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#contact">Contatos</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <a href="#about">Sobre</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#skills">Habilidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#projects">Projetos</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#contact">Contatos</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
+    </HeaderBar>
+  )
+}
 
 export default Header
