163208b0dc9c5a4578f358b5b8a0928036fd00b3
Gabriel Assenato <gabrielassenato@gmail.com>
2024-09-17 12:17:36 -0300

    :sparkles: feat(moreprojects): Adicionei descrição e links nos projetos do "mais projetos"

--- src/components/MoreProjects/index.tsx
+++ src/components/MoreProjects/index.tsx
@@ -5,7 +5,9 @@ type MoreProjectsProps = {
   items: {
     name: string
     img: string
-    imgTecnology?: string[]
+    description: string
+    linkToRepo: string
+    linkVisit: string
   }[]
 }
 
@@ -16,10 +18,17 @@ export const MoreProjects: React.FC<MoreProjectsProps> = ({ items }) => {
         {items.map((item, index) => (
           <li key={index}>
             <h2>{item.name}</h2>
-            <img src={item.img} alt={item.name} />
+            <div className="image-container">
+              <img src={item.img} alt={item.name} />
+              <div className="overlay">{item.description}</div>
+            </div>
             <div>
-              <Button>Acessar</Button>
-              <Button>Repositório</Button>
+              <a href={item.linkVisit} target="_blank" rel="noreferrer">
+                <Button>Acessar</Button>
+              </a>
+              <a href={item.linkToRepo} target="_blank" rel="noreferrer">
+                <Button>Repositório</Button>
+              </a>
             </div>
           </li>
         ))}
