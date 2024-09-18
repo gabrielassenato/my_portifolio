a4bd1d0af8080b09cecadd128bf158c4c8cf9268
Gabriel Assenato <gabrielassenato@gmail.com>
2024-09-18 01:53:59 -0300

    :iphone: feat(footer): Ajustes na responsividade

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
+import { breakpoints } from '../../styles'
 
 export const FooterBar = styled.div`
   background-color: #ddd;
   margin-top: 42px;
   text-align: center;
   padding: 18px;
+
+  @media (max-width: ${breakpoints.cellphone}) {
+    font-size: 14px;
+  }
 `
